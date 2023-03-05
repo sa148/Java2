3d885b38abf6f508a27bf9a994db67977a9d8b8f
sa148 <s.kurono@outlook.jp>
2023-03-05 22:28:55 +0900

    firstcommit

---
+++ 1/First.Comment.java
@@ -0,0 +1,9 @@
+/* FirstComment. java
+    文字列を表示するプログラム
+    2023.3.5                 */
+
+class FirstComment {
+  public static void main(String[] args) {
+    system.out.println("javaを楽しもう！"); //画面表示
+  }
+}
