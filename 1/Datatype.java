ea2acb72284bdaac9cf321c779113110fc587fe5
sa148 <s.kurono@outlook.jp>
2023-03-05 23:52:29 +0900

    addデータ型

---
+++ 1/Datatype.java
@@ -0,0 +1,17 @@
+class Datatype {
+  public static void main(String[] args) {
+    int month = 12;
+    int day = 10;
+    float weight = 63.00f;
+    double height = 168.0;
+    char bloodtype = 'A';
+
+    String name = "菅原";
+
+    System.out.println("こんにちは！" + name + "です");
+    System.out.println("身長は" + height + "cm, 体重は" + weight + "kg, ");
+    System.out.println("誕生日は" + month + day + "日、");
+    System.out.println("血液型は" + bloodtype + "型です。");
+    System.out.println("チャンネル登録お願いします");
+  }
+}
---
+++ 1/Variables.java
@@ -0,0 +1,9 @@
+class Variables {
+  public static void main(String[] args) {
+    int  month;
+    month = 4;
+    int day = 1;
+    System.out.println("お試し");
+    System.out.println(month + "月" + day + "日");
+  } 
+}
