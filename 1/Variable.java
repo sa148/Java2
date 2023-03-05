1dfbbdf9022d7bd1389585e8ac7b9dbb0717e1ed
sa148 <s.kurono@outlook.jp>
2023-03-05 23:07:53 +0900

    add変数

---
+++ 1/Variable.java
@@ -0,0 +1,10 @@
+class Variable {
+  public static void main(String[] args) {
+    int month;
+    month = 3;
+    int day = 5;
+
+    System.out.println("この勉強は");
+    System.out.println(month + "月" + day + "日から初めました" );
+  }
+}
