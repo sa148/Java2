8a7955c17bc80c636b8efbacb1887d0922d6c1bc
sa148 <s.kurono@outlook.jp>
2023-03-07 02:57:18 +0900

    add練習問題4

---
+++ 1/ScoreArray.java
@@ -0,0 +1,15 @@
+class ScoreArray{
+  public static void main(String[] args){
+    int [] date = new int[4];
+
+    date[0] = 90;
+    date[1] = 80;
+    date[2] = 100;
+    date[3] = 90;
+
+    System.out.println(date[0]);
+    System.out.println(date[1]);
+    System.out.println(date[2]);
+    System.out.println(date[3]);
+  }
+}
--- 1/ShowData.java
+++ 1/ShowData.java
@@ -3,13 +3,13 @@ class ShowData{
     int num = 10;
     double pi = 3.14;
     char blood = 'A';
-    //boolean fun = "true";
+    boolean fun = true;
     String msg = "いえ";
 
     System.out.println(num);
     System.out.println(pi);
     System.out.println(blood);
-    //System.out.println(fun);
+    System.out.println(fun);
     System.out.println(msg);
   }
 }
