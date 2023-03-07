0a3dfb7cc06ef1775c2ddda05ab7a41818832a80
sa148 <s.kurono@outlook.jp>
2023-03-08 02:08:39 +0900

    addfor文

--- 1/Loop1.java
+++ 1/Loop1.java
@@ -2,8 +2,7 @@ class Loop1{
   public static void main(String[] args){
     int i = 0;
     while(i < 3){
-      System.out.println("i = " + i);
-      i++;
+      System.out.println("i = " + i) ;
     }
   }
 }
