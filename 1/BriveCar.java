35bed1273e02543496d99591e079a5c3f4f3b0bb
sa148 <s.kurono@outlook.jp>
2023-03-09 02:13:51 +0900

    addオブジェクト復習

---
+++ 1/BriveCar.java
@@ -0,0 +1,11 @@
+class BriveCar{
+  public static void main(String[] args){
+    Bar c1 = new Bar();{
+      c1.setNo(2525);
+      c1.run(30);
+      System.out.println(Bar);
+      c1.stop(0);
+      System.out.println(Bar);
+    }
+  }
+}
