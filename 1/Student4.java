fa83d2fdeb5718f16e5196fc7867e8a3592af0a4
sa148 <s.kurono@outlook.jp>
2023-03-09 23:37:16 +0900

    addstatic3

--- 1/Student4.java
+++ 1/Student4.java
@@ -1,13 +1,13 @@
 class Student4 {
   String name;
-  int couter;
+  static int couter;
   
   Student4(String n){
     name = n;
     couter++;
     System.out.println(nmae + "さんをインスタンス化しました");
   }
-  void display(){
+  static void display(){
     System.out.println(couter + "人です");
   }
 }
