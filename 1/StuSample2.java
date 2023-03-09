443a7dcc8eab46f73effa3560315bd2832a5a5ce
sa148 <s.kurono@outlook.jp>
2023-03-09 22:45:38 +0900

    addオーバーロード修正

--- 1/StuSample2.java
+++ 1/StuSample2.java
@@ -4,10 +4,10 @@ class StuSample2{
     Student2 stu2 = new Student2();
 
     stu1.setData("菅原");
-    stu1.setData(90, 80);
+    stu1.setScore(90, 80);
     stu1.display();
 
-    stu2.setScore("村山", 100, 80);
+    stu2.setData("村山", 100, 80);
     stu2.display();
   }
 }
