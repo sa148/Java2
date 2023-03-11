0ebcdf55d34398d7b610fd9631690effd7682be5
sa148 <s.kurono@outlook.jp>
2023-03-12 02:07:07 +0900

    add継承

---
+++ 1/StuSample6.java
@@ -0,0 +1,11 @@
+public class StuSample6{
+  public static void main(String[] args){
+    Student6 stu = new Student6();
+
+    stu.setStuNo("菅原");
+    stu.display();
+
+    stu.setStuNo(1);
+    stu.displayStuNo();
+  }
+}
