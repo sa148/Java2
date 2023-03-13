2801cf517724a53023a7844bb622db30535299dc
sa148 <s.kurono@outlook.jp>
2023-03-13 14:58:36 +0900

    addfinal

---
+++ 1/Person4.java
@@ -0,0 +1,11 @@
+public final class Student4 extends Person4{
+  private String name;
+
+  public Person4(String name){
+    this.name = name;
+  }
+
+  public void display(){
+    System.out.println("名前 :" + name);
+  }
+}
--- 1/StuSample4.java
+++ 1/StuSample4.java
@@ -1,9 +1,9 @@
-class StuSample4 {
+public class StuSample4 {
   public static void main(String[] args){
-    Student4 stu1 = new Student4("菅原");
-    Student4.display();
-
-    Student4 stu2 = new Student4("村山");
-    Student4.display();
+    final int BASE_NO = 1000;
+    Student4 stu1 = new Student4("菅原",BASE_NO+1);
+    stu1.display();
+    Student4 stu2 = new Student4("村山",BASE_NO+2);
+    stu2.display();
   }
 }
