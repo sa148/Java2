81986b85e0c0290725cfd7a9d0f411af4ba34586
sa148 <s.kurono@outlook.jp>
2023-03-16 03:37:39 +0900

    addインターフェース練習問題

--- 1/Bus.java
+++ 1/Bus.java
@@ -1,9 +1,13 @@
-public class Bus extends Vehicle{
+public class Bus extends Vehicle implements Stopable{
   public Bus(int crewNum){
     this.crewNum = crewNum;
   }
 
   public void showcrewNum(){
     System.out.println("バスの乗客:" + crewNum + "名");
+  }
+
+  public void stop(){
+    System.out.println("次止まります");
   }
 }
--- 1/taxi3.java
+++ 1/taxi3.java
@@ -1,9 +1,13 @@
-public class taxi3 extends Vehicle{
+public class taxi3 extends Vehicle implements Stopable{
   public taxi3(int crewNum){
     this.crewNum = crewNum;
   }
 
   public void showcrewNum(){
     System.out.println("タクシーの乗客:" + crewNum + "名");
+  }
+
+  public void stop(){
+    System.out.println("ここで降ります");
   }
 }
