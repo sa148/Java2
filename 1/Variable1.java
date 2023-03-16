e71afa8ce1a3e8e6c9e30ff551b6f3b4b37e1c33
sa148 <s.kurono@outlook.jp>
2023-03-17 02:59:02 +0900

    addJava基本振り返り２

---
+++ 1/Variable1.java
@@ -0,0 +1,10 @@
+public class Variable1{
+  public static void main(String[] args){
+    int mouth;
+    mouth = 9;
+    int day = 16;
+
+    System.out.println("このチャンネルは、");
+    System.out.println(mouth + "月" + day + "日に公開しました");
+  }
+}
