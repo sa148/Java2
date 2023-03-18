04516e06782208d615e38eda2e5480718e87161c
sa148 <s.kurono@outlook.jp>
2023-03-19 03:45:04 +0900

    addJava基本振り返り11

--- 1/Branch2.java
+++ 1/Branch2.java
@@ -1,19 +1,19 @@
 class Branch2{
   public static void main(String[] args){
     int price = Integer.parseInt(args[0]);
-    double rate = 0.10;       //消費税率　: 10％
+    double rate = 0.10;
     int discount, amount;
 
-    if(price >= 5000 ){
+    if(price >= 5000){
       discount = 500;
-    } else if(price >= 3000 ){
+    } else if(price >= 3000){
       discount = 300;
-    } else {
+    } else{
       discount = 0;
     }
 
     amount = (int)((price - discount) * (1 + rate));
-    System.out.println("値引き金額 :" + discount + "円");
-    System.out.println("税込金額 :" + amount + "円");
+    System.out.println("値引金額" + discount + "円");
+    System.out.println("税込金額" + amount + "円");
   }
 }
