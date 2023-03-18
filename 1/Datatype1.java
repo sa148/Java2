c0806242dfeb8c53c67ad29ff8c0677babb81285
sa148 <s.kurono@outlook.jp>
2023-03-18 19:05:18 +0900

    addJava基本振り返り３

---
+++ 1/Datatype1.java
@@ -0,0 +1,18 @@
+class Datatype1{
+ public static void main(String[] args){
+  int mouth = 12;
+  int day = 9;
+  float weight = 63.0f;
+  double height = 168.5;
+  char bloodtype = 'A';
+
+  String name = "菅原";
+
+  System.out.println("こんにちは" + name + "です");
+  System.out.println("身長は" + height + "cm, 体重は" + weight + "kg,");
+  System.out.println("誕生日は" + mouth + "月" + day + "日");
+  System.out.println("血液型" + bloodtype + "型です。");
+  System.out.println("こんにちは" + name + "です");
+  System.out.println("チャンネルや拡散");
+ } 
+}
