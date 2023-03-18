100a383602d11c02831cc0a84f93ef462755c1ed
sa148 <s.kurono@outlook.jp>
2023-03-19 03:13:52 +0900

    addJava基本振り返り9

---
+++ 1/Cast1.java
@@ -0,0 +1,10 @@
+class Cast1 {
+  public static void main(String[] args){
+    int price = Integer.parseInt(args[0]);
+    double rate = 0.08;
+    int amount;
+
+    amount = (int)(price * (1 + rate));
+    System.out.println("税込金額" + amount + "円です");
+  }
+}
