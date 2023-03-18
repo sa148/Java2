ecc73d55603faaf1303554694de31c7d18f52976
sa148 <s.kurono@outlook.jp>
2023-03-19 00:24:44 +0900

    addJava基本振り返り7

---
+++ 1/ShowData1.java
@@ -0,0 +1,15 @@
+class ShowData1 {
+  public static void main(String[] args){
+    int num = 10;
+    double pi = 3.14;
+    char blood = 'A';
+    boolean fun = true;
+    String msg = "練習";
+    
+    System.out.println(num);
+    System.out.println(pi);
+    System.out.println(blood);
+    System.out.printlm(fun);
+    System.out.println(msg);
+  }
+}
