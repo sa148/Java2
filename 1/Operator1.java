c72eff781c2e7e55ecf80fa1df556cc5559de50e
sa148 <s.kurono@outlook.jp>
2023-03-19 02:57:11 +0900

    addJava基本振り返り8

---
+++ 1/Operator1.java
@@ -0,0 +1,15 @@
+class Operator1 {
+  public static void main(String[] args){
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+
+    System.out.println("a = " + a + ", b=" + b);
+    System.out.println("a + b " + (a + b));
+    System.out.println("a - b " + (a - b));
+    System.out.println("a * b " + (a * b));
+    System.out.println("a / b " + (a / b));
+    System.out.println("a % b " + (a % b));
+    System.out.println("a >= b " + (a >= b));
+    System.out.println("(a >= 0) && (b>=0) :" + ((a >= 0) && (b >= 0)));
+  }
+}
