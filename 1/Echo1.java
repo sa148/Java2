91c2175e7be32a0b0723559f5f6b5d7fe27a6c26
sa148 <s.kurono@outlook.jp>
2023-03-19 17:26:15 +0900

    addJava基本振り返り15

---
+++ 1/Echo1.java
@@ -0,0 +1,7 @@
+class Echo1 {
+  public static void main(String[] args){
+    for(int i = 0; i < 3; i++){
+      
+    }
+  }  
+}
---
+++ 1/LoopMessage1.java
@@ -0,0 +1,9 @@
+class LoopMessage1 {
+  public static void main(String[] args){
+    int i = 0;
+    while(i < 11){
+      System.out.println(i + "回目:" + "練習");
+      i++;
+    }
+  }
+}
