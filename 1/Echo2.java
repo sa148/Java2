2159997fe1a450db70acfdcc41ce4d5f50bb4e2a
sa148 <s.kurono@outlook.jp>
2023-03-21 06:13:47 +0900

    addオブジェクト指向復習3

--- 1/Echo2.java
+++ 1/Echo2.java
@@ -1,6 +1,6 @@
 class Echo2 {
   public static void main(String[] args){
-    for(i = 0; i > args.length; i++){
+    for(int i = 0; i > args.length; i++){
       System.out.printlan(args[i]);
     }
   }  
