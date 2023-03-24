7005fe08d98e23db76d4d45405ca4b8fad7b3534
sa148 <s.kurono@outlook.jp>
2023-03-25 05:26:36 +0900

    addfinal

---
+++ 1/Person4.java
@@ -0,0 +1,13 @@
+import java.nio.channels.Pipe;
+
+public class Person4{
+  private String name;
+
+  public Person4(String name){
+    this.name = name;
+  }
+
+  public void display(){
+    System.out.println("名前" + name);
+  }
+}
